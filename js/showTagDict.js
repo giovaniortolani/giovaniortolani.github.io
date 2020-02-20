e80373ffbe68f7db9545fc24cab9ec2661dd4ada
Giovani Ortolani Barbosa <giovaniortolanibarbosa@gmail.com>
2020-02-20 10:20:48 -0300

    URL showTagDict removida

--- js/showTagDict.js
+++ js/showTagDict.js
@@ -3,8 +3,7 @@ const callSpreadsheet = async () => {
     /** Usando a versão /dev, que em tese deve refletir as mudanças sem precisar dar deploy em uma nova versão.
      *  'https://script.google.com/macros/s/AKfycbwnpSQT3-q4JxhgPZKuos9WyyzDu3dnAMyOcQRR3G7FVLNuNRZ1/exec'
      */
-    //'https://script.google.com/macros/s/AKfycbwnpSQT3-q4JxhgPZKuos9WyyzDu3dnAMyOcQRR3G7FVLNuNRZ1/dev'
-    'https://script.googleusercontent.com/a/macros/raccoon.ag/echo?user_content_key=BE8AkjtS-VzSIU6KdZpkmdvOAyBc8f4Loa3sPnyXnILOyH_Zv50NR-kho9NqRlravwCIS_B3vy85FOquWWglWrTYm_0s2hAGm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_znu-QG1bDc2kc3c3WduKtKRaZ5dCdzsK_cPt-9sL2sEWkxSqzGfEdXzM1UAKByClN360qsyesEPbS1ZKgmGnnaSZRe-_Z6h7hlgo3jS_cH4&lib=MkuCK64gRAHvDYl9-WgKMUX22WVp6P5tO'
+    'https://script.google.com/macros/s/AKfycbwnpSQT3-q4JxhgPZKuos9WyyzDu3dnAMyOcQRR3G7FVLNuNRZ1/dev'
   );
   const responseJson = await response.json();
   const showTagDict = JSON.parse(responseJson);
