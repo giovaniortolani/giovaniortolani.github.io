97831f72e36e164e61195ab535eac204c5363520
Giovani Ortolani Barbosa <giovaniortolanibarbosa@gmail.com>
2020-04-15 17:32:04 -0300

    debugger.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', event => {
+(function () {
+  document.addEventListener('DOMContentLoaded', (event) => {
     console.log('%c> Page State:', 'font-weight:bold;color:red', 'DOM loaded.');
   });
 
-  document.addEventListener('readystatechange', event => {
+  document.addEventListener('readystatechange', (event) => {
     if (event.target.readyState === 'interactive') {
       console.log('%c> Page State:', 'font-weight:bold color:red', 'readystate interactive.');
     }
@@ -13,25 +13,25 @@
     }
   });
 
-  window.addEventListener('load', event => {
+  window.addEventListener('load', (event) => {
     console.log('%c> Page State:', 'font-weight:bold;color:red', 'Window loaded.');
   });
 })();
 
-(function() {
-  window.pushToDataLayer = function(event, eventParams) {
+(function () {
+  window.pushToDataLayer = function (event, eventParams) {
     if (!eventParams.eventCallback) eventParams.eventCallback = () => {};
     if (!eventParams.eventTimeout) eventParams.eventTimeout = 4000;
 
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
       event: event,
-      ...eventParams
+      ...eventParams,
     });
   };
 
-  window.callGoogleWebApp = async function() {
-    const event = 'googleWebApp';
+  window.callGoogleWebApp = async function (event) {
+    const dlEvent = 'googleWebApp';
     const queryString = 'action=quotes&cacheexpiration=' + Math.ceil(5 + Math.random() * 15); // 5 a 20 segundos
     const url = `https://script.google.com/macros/s/AKfycbwLu-Elv5pC10ehajglL7gq6rkB8asH_LWkBmUFUtYGI2yA9csR/exec?${queryString}`;
     const div = document.querySelector('#googleWebApp');
@@ -42,21 +42,21 @@
       const response = await fetch(url);
       if (!response.ok) {
         data = 'Error fetching Google Web App.';
-        pushToDataLayer(event, { status: 'Error' });
+        pushToDataLayer(dlEvent, { status: 'Error' });
       } else {
         try {
           data = await response.json();
-          pushToDataLayer(event, { status: 'OK', ...data });
+          pushToDataLayer(dlEvent, { status: 'OK', ...data });
         } catch (err) {
           data = err.message;
-          pushToDataLayer(event, { status: 'Error', ...data });
+          pushToDataLayer(dlEvent, { status: 'Error', ...data });
         }
       }
     } catch (e) {
       data = 'Google Web App request failed.';
-      pushToDataLayer(event, { status: 'Error', ...data });
+      pushToDataLayer(dlEvent, { status: 'Error', ...data });
     }
-    this.textContent += ' - *';
+    event.target.textContent += '*';
     div.textContent = data.text;
   };
 })();
